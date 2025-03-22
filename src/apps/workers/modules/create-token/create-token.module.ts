1d184bd18a68d4aadf95cb523f5459bf2409b59d
Aciilac <vitvitskyi.vd@gmail.com>
2025-03-22 16:53:54 +0100

    fix/ worker

--- src/apps/workers/modules/create-token/create-token.module.ts
+++ src/apps/workers/modules/create-token/create-token.module.ts
@@ -1,9 +1,10 @@
 import { BullModule } from '@nestjs/bull'
 import { Module } from '@nestjs/common'
 import { CREATE_TOKEN } from 'src/common/constants/queues'
+import { CreateTokenService } from './create-token.service'
 
 @Module({
-  providers: [],
+  providers: [CreateTokenService],
   imports: [
     BullModule.registerQueue({
       name: CREATE_TOKEN,
