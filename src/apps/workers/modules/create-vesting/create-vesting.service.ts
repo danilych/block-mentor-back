743300e6c97137065078f8bc522e51faea1a48e2
danilych <danilych45@proton.me>
2025-03-23 07:25:14 +0100

    gwip

--- src/apps/workers/modules/create-vesting/create-vesting.service.ts
+++ src/apps/workers/modules/create-vesting/create-vesting.service.ts
@@ -101,7 +101,7 @@ export class CreateVestingService {
           caip2: `eip155:${ESupportedChains[components.chain]}`,
           transaction: {
             value: Number(0),
-            chainId: ESupportedChains[components.chain],
+            chainId: +ESupportedChains[components.chain],
             to: ETokenFactoryDeployments[components.chain],
             data: encodedData,
           },
