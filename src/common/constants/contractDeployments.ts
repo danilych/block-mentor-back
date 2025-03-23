5298aa450b6fcb044d3f24d8203ca641c2b05e00
danilych <danilych45@proton.me>
2025-03-23 09:02:34 +0100

    chore: add base contracts

--- src/common/constants/contractDeployments.ts
+++ src/common/constants/contractDeployments.ts
@@ -1,5 +1,6 @@
 export enum ETokenFactoryDeployments {
   arbitrum = '0x114b314f3925a681ed729743b31f92dd157Ff2c5',
+  base = '0x108CC1D614ce056e890911B24f1550307a119708',
 }
 
 export enum ETokenVestingFactoryDeployments {
